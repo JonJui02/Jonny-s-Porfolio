ba970512b7fc485216b89735bab1ead260ee473a
Jonathan Juining <73358104+FriesKnight@users.noreply.github.com>
2024-05-30 20:30:40 +0800

    yes

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import styles from "./Experience.module.css";
 import skills from "../../data/skills.json";
 import history from "../../data/history.json";
+import certifications from "../../data/certifications.json"; // Import certifications data
 import { getImageUrl } from "../../utils";
 
 export const Experience = () => {
@@ -44,6 +45,37 @@ export const Experience = () => {
           })}
         </ul>
       </div>
+
+      <section className={styles.certifications} id="certifications">
+        <h2 className={styles.title}>Certifications</h2>
+        <div className={styles.certificationList}>
+          {certifications.map((certification, id) => {
+            return (
+              <a
+                key={id}
+                href={certification.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.certificationLink}
+              >
+                <div className={styles.certification}>
+                  <div className={styles.certificationImageContainer}>
+                    <img
+                      src={getImageUrl(certification.imageSrc)}
+                      alt={certification.title}
+                    />
+                  </div>
+                  <div className={styles.certificationDetails}>
+                    <h3>{certification.title}</h3>
+                    <p>{certification.issuingOrganization}</p>
+                    <p>{certification.issueDate}</p>
+                  </div>
+                </div>
+              </a>
+            );
+          })}
+        </div>
+      </section>
     </section>
   );
 };
